5702f68e4718607d5830b17e78507305784734ac
vali8055 <mahaboobvali8055@gmail.com>
2022-02-13 19:30:07 +0530

    Updates

---
+++ precache-manifest.de5445129095b385a7508cc8c7edc80b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c611c0fcb1feeaeb330c",
+    "url": "/react/static/css/main.c164344e.chunk.css"
+  },
+  {
+    "revision": "c611c0fcb1feeaeb330c",
+    "url": "/react/static/js/main.83865d37.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "71d7194958f9367e0e6e",
+    "url": "/react/static/js/2.de6ff777.chunk.js"
+  },
+  {
+    "revision": "c7bb6f2c3b44e88949bd6086a53ad26f",
+    "url": "/react/index.html"
+  }
+];
